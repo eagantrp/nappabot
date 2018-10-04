05478b159dab200169f0852bcc3b2534e6fef955
eagantrp <eagant@nlcs.k12.in.us>
2018-10-04 12:09:41 -0400

    began nappabot.js

---
+++ nappabot.js
@@ -0,0 +1,16 @@
+import Discord from 'discord.js'
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log("I am ready!")
+})
+
+client.on('message', message => {
+    if (message.content === '!Hello') {
+        message.channel.send('Hello, ' + message.author)
+    }
+
+})
+
+client.login('497437524738965514')
